883808bc1a70c41d075ed66807f0f1fb2c3d9c51
hashishrajan <ashishprajan@gmail.com>
2023-09-30 00:18:56 +0100

    Terraform to Create & Enable AWS CloudTrail

---
+++ enable-cloud-trail/main.tf
@@ -0,0 +1,14 @@
+#this is the main declaration file for AWS Provider using Terraform's Latest version as of Sep,2023
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.19.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+  #profile = "default""
+}
