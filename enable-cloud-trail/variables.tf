a0416fc310a45c430285b2b4e60d031098c719df
hashishrajan <ashishprajan@gmail.com>
2023-09-30 00:51:41 +0100

    fixed typo

--- enable-cloud-trail/variables.tf
+++ enable-cloud-trail/variables.tf
@@ -1,19 +1,19 @@
 #Variables to be used for this folder
 
 variable "aws_region" {
-  Description = "Region of AWS"
+  description = "Region of AWS"
   #default = "us-east-1"
   type = string
 }
 
 variable "cloudtrail_bucket_name" {
-  Description = "Bucket name to store CloudTrial Logs"
+  description = "Bucket name to store CloudTrial Logs"
   #default = "CloudTrail Bucket Name"
   type = string
 }
 
 variable "cloudtrail_name" {
-  Description = "Name of CloudTrial"
+  description = "Name of CloudTrial"
   #default = "CloudTrail  Name"
   type = string
 }
