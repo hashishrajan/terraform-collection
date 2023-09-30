c73f9b82095e3c982d79bf4a75e7cc3afce387ba
hashishrajan <ashishprajan@gmail.com>
2023-09-30 01:01:26 +0100

    fixed variable

--- enable-cloud-trail/create-cloudtrail.tf
+++ enable-cloud-trail/create-cloudtrail.tf
@@ -1,12 +1,12 @@
 #declare resources that should be created in AWS Cloud
-resource "aws_s3_bucket" "cloudtrail_bucket" {
+resource "aws_s3_bucket" "example_bucket" {
   bucket        = var.cloudtrail_bucket_name
   force_destroy = true
 }
 
 resource "aws_cloudtrail" "main_cloudtrail" {
   name                          = var.cloudtrail_name
-  s3_bucket_name                = aws_s3_bucket.cloudtrail_bucket.bucket
+  s3_bucket_name                = aws_s3_bucket.example_bucket.bucket
   enable_logging                = true
   include_global_service_events = true
 }
