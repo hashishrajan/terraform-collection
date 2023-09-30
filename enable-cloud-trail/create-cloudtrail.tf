db5f80aa77a6edc4a77269ad1efcdd79709119d0
hashishrajan <ashishprajan@gmail.com>
2023-09-30 13:29:31 +0100

    revert variable change again

--- enable-cloud-trail/create-cloudtrail.tf
+++ enable-cloud-trail/create-cloudtrail.tf
@@ -10,7 +10,7 @@ resource "aws_s3_bucket" "cloudtrail_bucket" {
 
 resource "aws_cloudtrail" "main_cloudtrail" {
   name                          = var.cloudtrail_name
-  s3_bucket_name                = aws_s3_bucket.cloudtrail_bucket.bucket
+  s3_bucket_name                = "cloudtrail-bucket-30sep2023"
   s3_key_prefix                 = "prefix"
   enable_logging                = true
   include_global_service_events = true
