230a48ebb62939b35f99db3646c1a12b84384895
hashishrajan <ashishprajan@gmail.com>
2023-09-30 13:26:17 +0100

    revert variable change

--- enable-cloud-trail/create-cloudtrail.tf
+++ enable-cloud-trail/create-cloudtrail.tf
@@ -5,40 +5,6 @@ data "aws_caller_identity" "current" {}
 resource "aws_s3_bucket" "cloudtrail_bucket" {
   bucket        = var.cloudtrail_bucket_name
   force_destroy = true
-
-    policy = <<POLICY
-
-    {}
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "AWSCloudTrailAclCheck20150319",
-            "Effect": "Allow",
-            "Principal": {"Service": "cloudtrail.amazonaws.com"},
-            "Action": "s3:GetBucketAcl",
-            "Resource": "arn:aws:s3:::var.cloudtrail_bucket_name",
-            "Condition": {
-                "StringEquals": {
-                    "aws:SourceArn": "arn:aws:cloudtrail:region:$(data.aws_caller_identity.current.account_id):trail/var.cloudtrail_name"
-                }
-            }
-        },
-        {
-            "Sid": "AWSCloudTrailWrite20150319",
-            "Effect": "Allow",
-            "Principal": {"Service": "cloudtrail.amazonaws.com"},
-            "Action": "s3:PutObject",
-            "Resource": "arn:aws:s3:::var.cloudtrail_bucket_name/prefix/AWSLogs/$(data.aws_caller_identity.current.account_id)/*",
-            "Condition": {
-                "StringEquals": {
-                    "s3:x-amz-acl": "bucket-owner-full-control",
-                    "aws:SourceArn": "arn:aws:cloudtrail:region:$(data.aws_caller_identity.current.account_id):trail/var.cloudtrail_name"
-                  }
-                }
-        }
-        ]
-      }
-POLICY
 }
 
 
